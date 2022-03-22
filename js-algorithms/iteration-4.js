ef415b006d871912ad97c181fed2bdd072f384ba
Pablo Jared Ruz <96743254+pjaredruz@users.noreply.github.com>
2022-03-22 19:25:22 +0100

    Update iteration-4.js

--- js-algorithms/iteration-4.js
+++ js-algorithms/iteration-4.js
@@ -5,5 +5,18 @@ const avenger = avengers[0];
 console.log(avenger);
 
 //1.2
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+avenger = avengers[0];
+console.log(avenger);
+avengers.splice(0, 1, "IRONMAN");
+console.log(avengers);
+
+
+//1.3
+
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"]
+rickAndMortyCharacters.push("Morty", "Summer", "Lapiz Lopez");
+rickAndMortyCharacters.pop();
+console.log(rickAndMortyCharacters);
 
 
